a802aca74e1d133e653c14e4dbb403cd99d6cbf2
Rhutasel <loganrhutasel@gmail.com>
2019-03-26 15:54:15 -0500

    Update website.js

--- config/website.js
+++ config/website.js
@@ -1,25 +1,25 @@
 const tailwind = require('../tailwind')
 
 module.exports = {
-  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
+  pathPrefix: '/',   // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteTitleShort: 'Cara', // short_name for manifest
-  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
-  siteUrl: 'https://cara.lekoarts.de', // Domain of your site. No trailing slash!
-  siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: '/logo.png', // Used for SEO and manifest
-  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  siteTitle      : 'Logan Rhutasel Portfolio',          // Navigation and Site Title
+  siteTitleAlt   : 'Rhutasel',                          // Alternative Site title for SEO
+  siteTitleShort : 'Rhutasel',                          // short_name for manifest
+  siteHeadline   : 'Web Designer | Web Developer',      // Headline for schema.org JSONLD
+  siteUrl        : 'https://rhutasel.dev',              // Domain of your site. No trailing slash!
+  siteLanguage   : 'en',                                // Language Tag on <html> element
+  siteLogo       : '/logo.png',                         // Used for SEO and manifest
+  siteDescription: 'Logan Rhutasel Personal Portfolio,
+  author: 'LekoArts',   // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@cara', // Twitter Username
-  ogSiteName: 'cara', // Facebook Site Name
-  ogLanguage: 'en_US', // Facebook Language
+  //userTwitter      : '@cara',           // Twitter Username
+  ogSiteName       : 'Logan Rhutasel',   // Facebook Site Name
+  ogLanguage       : 'en_US',            // Facebook Language
   googleAnalyticsID: 'UA-47519312-5',
 
   // Manifest and Progress color
-  themeColor: tailwind.colors.orange,
+  themeColor     : tailwind.colors.orange,
   backgroundColor: tailwind.colors.blue,
 }
